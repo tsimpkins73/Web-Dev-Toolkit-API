ef50b0c9b3e27a17e037fa3f235453f7e5d4e7d5
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2020-01-12 22:26:39 -0500

    Environment Setup

--- src/config.js
+++ src/config.js
@@ -6,9 +6,9 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'token',
-    API_BASE_URL: 'the-console.herokuapp.com/api',
-    DB_URL: process.env.DB_URL || "postgres://fyxberjbsmgram:3b5dd74bbacb752ce6970bdcc42b81e4e0594683dcdc4d8e22731a9a8e1760e4@ec2-174-129-254-217.compute-1.amazonaws.com:5432/de7a6qnferjba2",
+    API_BASE_URL: 'web-dev-toolkit-api.herokuapp.com/',
+    DB_URL: process.env.DB_URL || "postgres://bjdtgbaxupkxmh:2b6cacf62d5382f61d8f543d7d320bbb75a9e010e98b37adc725f5d4182e46b6@ec2-174-129-33-2.compute-1.amazonaws.com:5432/d7oq5p5pdkm8fk",
     JWT_SECRET: process.env.JWT_SECRET || 'WebDevToolKit',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '2h',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://theconsole-thankful-cassowary.now.sh/',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://web-dev-toolkit-client.t73designs.now.sh/',
   }
