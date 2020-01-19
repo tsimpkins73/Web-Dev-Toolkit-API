7e3301daafb4ffc07e5b5fdc74356d5bd7d11b14
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2020-01-19 17:34:13 -0500

    Config

--- src/config.js
+++ src/config.js
@@ -10,5 +10,5 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL || 'postgres://bjdtgbaxupkxmh:2b6cacf62d5382f61d8f543d7d320bbb75a9e010e98b37adc725f5d4182e46b6@ec2-174-129-33-2.compute-1.amazonaws.com:5432/d7oq5p5pdkm8fk',
     JWT_SECRET: process.env.JWT_SECRET || 'WebDevToolKit',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '2h',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://web-dev-toolkit-client.now.sh/',
   }
