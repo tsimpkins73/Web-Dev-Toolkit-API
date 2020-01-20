d9ea095356f7a80e0f715a6ab60a8aa4c9ef70c8
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2020-01-19 19:07:52 -0500

    Config

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'token',
     API_BASE_URL: process.env.API_BASE_URL || 'https://web-dev-toolkit-api.herokuapp.com/api',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://bjdtgbaxupkxmh:2b6cacf62d5382f61d8f543d7d320bbb75a9e010e98b37adc725f5d4182e46b6@ec2-174-129-33-2.compute-1.amazonaws.com:5432/d7oq5p5pdkm8fk',
+    DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET || 'WebDevToolKit',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '2h',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://web-dev-toolkit-client.now.sh',
